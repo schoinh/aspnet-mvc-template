9106f465e677ba809065d539b17debbb2c8df8a0
Na Hyung Choi <schoinh@gmail.com>
2019-07-31 10:58:38 -0700

    update MVC stuff

--- ProjectName/Program.cs
+++ ProjectName/Program.cs
@@ -1,10 +1,20 @@
-using System;
-using NAMESPACE;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
-class Program
+namespace ProjectName
 {
-    private static void Main()
+    public class Program
     {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
 
+            host.Run();
+        }
     }
 }
