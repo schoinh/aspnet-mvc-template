179ca12c2d66099d7e0317661b2f0feaaa4a3dca
Na Hyung Choi <schoinh@gmail.com>
2019-08-01 10:53:05 -0700

    add static files method

--- ProjectName/Startup.cs
+++ ProjectName/Startup.cs
@@ -23,12 +23,16 @@ namespace ProjectName
         public void Configure(IApplicationBuilder app)
         {
             app.UseDeveloperExceptionPage();
+            
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name:"default",
                     template:"{controller=Home}/{action=Index}/{id}");
             });
+            
+            app.UseStaticFiles();
+            
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("something went wrong!");
